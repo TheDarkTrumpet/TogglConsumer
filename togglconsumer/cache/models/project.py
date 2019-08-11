707e42e79b0dfd3ac3cdc05d37ae1256d96499d3
David Thole <david@thedarktrumpet.com>
2019-08-11 17:36:32 -0500

    Adding some more properties to be tracked

--- togglconsumer/cache/models/project.py
+++ togglconsumer/cache/models/project.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Boolean
 from togglconsumer.cache.models.base import Base
 
 
@@ -7,8 +7,11 @@ class Project(Base):
 
     id = Column(Integer, primary_key=True, autoincrement=True)
     workspace_id = Column(Integer)
-
     name = Column(String)
-
+    billable = Column(Boolean)
+    is_private = Column(Boolean)
+    active = Column(Boolean)
+    actual_hours = Column(Integer)
+    
     def __repr__(self):
         return "<Project(id='%i' name='%s)>" % (self.id, self.name)
