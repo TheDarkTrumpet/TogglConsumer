242411df1d2d065d58fc668f76b853f30f3ab857
David Thole <david@thedarktrumpet.com>
2019-08-11 19:29:03 -0500

    Finished writing the first migration

--- togglconsumer/cache/migrations/versions/001_Create_base_project_table.py
+++ togglconsumer/cache/migrations/versions/001_Create_base_project_table.py
@@ -1,3 +1,4 @@
+from migrate.tests.fixture.models import meta
 from sqlalchemy import *
 from migrate import *
 
@@ -5,7 +6,16 @@ from migrate import *
 def upgrade(migrate_engine):
     # Upgrade operations go here. Don't create your own engine; bind
     # migrate_engine to your metadata
-    pass
+    projects = Table(
+        'project', meta,
+        Column('id', Integer, primary_key=True, autoincrement=true),
+        Column('workspace_id', Integer),
+        Column('name', String(255)),
+        Column('billable', Boolean),
+        Column('is_private', Boolean),
+        Column('active', Boolean),
+        Column('actual_hours', Integer)
+    )
 
 
 def downgrade(migrate_engine):
