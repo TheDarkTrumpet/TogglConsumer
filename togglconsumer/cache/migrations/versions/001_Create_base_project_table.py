b88347d743d6648011c1ae25270d5eff0e08a6b8
David Thole <david@thedarktrumpet.com>
2019-08-11 19:57:12 -0500

    Fixing the migration

--- togglconsumer/cache/migrations/versions/001_Create_base_project_table.py
+++ togglconsumer/cache/migrations/versions/001_Create_base_project_table.py
@@ -2,22 +2,25 @@ from migrate.tests.fixture.models import meta
 from sqlalchemy import *
 from migrate import *
 
+meta = MetaData()
+projects = Table(
+    'project', meta,
+    Column('id', Integer, primary_key=True, autoincrement=true),
+    Column('workspace_id', Integer),
+    Column('name', String(255)),
+    Column('billable', Boolean),
+    Column('is_private', Boolean),
+    Column('active', Boolean),
+    Column('actual_hours', Integer)
+)
 
 def upgrade(migrate_engine):
     # Upgrade operations go here. Don't create your own engine; bind
     # migrate_engine to your metadata
-    projects = Table(
-        'project', meta,
-        Column('id', Integer, primary_key=True, autoincrement=true),
-        Column('workspace_id', Integer),
-        Column('name', String(255)),
-        Column('billable', Boolean),
-        Column('is_private', Boolean),
-        Column('active', Boolean),
-        Column('actual_hours', Integer)
-    )
-
+    meta.bind = migrate_engine
+    projects.create()
 
 def downgrade(migrate_engine):
     # Operations to reverse the above upgrade go here.
-    pass
+    meta.bind = migrate_engine
+    projects.drop()
