6c3398a5d309cfc54d5ec39b7565d4003a5bfbdb
David Thole <david@thedarktrumpet.com>
2019-08-13 11:33:23 -0500

    Adding dynamic attribute assignment

--- togglconsumer/cache/models/project.py
+++ togglconsumer/cache/models/project.py
@@ -1,4 +1,5 @@
 from sqlalchemy import Column, Integer, String, Boolean
+
 from togglconsumer.cache.models.base import Base
 
 
@@ -13,5 +14,14 @@ class Project(Base):
     active = Column(Boolean)
     actual_hours = Column(Integer)
 
+    def __init__(self, input_dictionary=None):
+        if input_dictionary is not None:
+            self.set_attributes(input_dictionary)
+
     def __repr__(self):
         return "<Project(id='%i' name='%s)>" % (self.id, self.name)
+
+    def set_attributes(self, input_dictionary):
+        for key, value in input_dictionary.iteritems():
+            if self.__getattribute__(key) is not None:
+                setattr(self, key, value)
