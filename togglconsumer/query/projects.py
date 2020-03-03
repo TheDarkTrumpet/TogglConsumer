d4af5d74833d7abdd5a248f9308b6ad2dc8970b4
David Thole <david@thedarktrumpet.com>
2020-03-03 09:52:35 -0600

    Adding a new class for the individual projects that'll be the enumerable part of all this

--- togglconsumer/query/projects.py
+++ togglconsumer/query/projects.py
@@ -14,4 +14,20 @@ class Projects (base):
         return json.loads(responseText.text)
 
 
-print(GetProjects())
+
+class Project:
+    def __init__(self):
+        self.id = 0
+        self.wid = 0
+        self.cid = 0
+        self.name = ""
+        self.billable = ""
+        self.is_private = ""
+        self.active = False
+        self.template = False
+        self.at = 0
+        self.created_at = 0
+        self.color = 0
+        self.auto_estimates = False
+        self.actual_hours = 0
+        self.hex_color = 0
