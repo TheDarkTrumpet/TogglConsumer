efea7b201dade67dc26a91d6b5d1c458cb940a5b
David Thole <david@thedarktrumpet.com>
2020-03-03 15:42:03 -0600

    Adding workspace.py and the the model it should represent

--- togglconsumer/cache/models/workspace.py
+++ togglconsumer/cache/models/workspace.py
@@ -2,26 +2,27 @@ from sqlalchemy import Column, Integer, String, Boolean, DateTime, DECIMAL
 from togglconsumer.cache.models.base import Base
 
 
-class Project(Base):
-    __tablename__ = 'Projects'
+class Workspace(Base):
+    __tablename__ = 'Workspaces'
 
     id = Column(Integer, primary_key=True)
-    wid = Column('workspace_id', Integer)
-    cid = Column(Integer)
     name = Column(String(255))
-    billable = Column(Boolean)
-    is_private = Column(Boolean)
-    active = Column(Boolean)
-    template = Column(Boolean)
+    profile = Column(Integer)
+    premium = Column(Boolean)
+    admin = Column(Boolean)
+    default_hourly_rate = Column(Integer)
+    default_currency = Column(String(20))
+    only_admins_may_create_projects = Column(Boolean)
+    only_admins_see_billable_rates = Column(Boolean)
+    only_admins_ee_team_dashboard = Column(Boolean)
+    project_bllable_by_default = Column(Boolean)
+    rounding = Column(Integer)
+    rounding_minutes = Column(Integer)
+    api_token = Column(String(50))
     at = Column(DateTime)
-    created_at = Column(DateTime)
-    color = Column(Integer)
-    auto_estimates = Column(Boolean)
-    actual_hours = Column(Integer)
-    hex_color = Column(String(20))
-    rate = Column(DECIMAL)
-    guid = Column(String(50))
-    currency = Column(String(20))
+    logo_url = Column(String(500))
+    ical_url = Column(String(500))
+    ical_enabled = Column(Boolean)
 
     def __init__(self, input_dictionary=None):
         if input_dictionary is not None:
