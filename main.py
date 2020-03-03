65fe72bc7a1725a3b31fa79426d6748390fc681a
David Thole <david@thedarktrumpet.com>
2020-03-03 16:27:15 -0600

    Adding some debug code for for times, through projects

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from togglconsumer.query.projects import *
 from togglconsumer.query.workspaces import *
+from datetime import date
+
 
 p = Projects()
 p.load()
@@ -8,7 +10,10 @@ w = Workspaces()
 w.load()
 
 
-
+start_date =  date.fromisoformat('2020-01-01')
+stop_date = date.fromisoformat('2020-02-03')
+client = Settings().toggl_client
+responseText = client.get_project_times(155621750, start_date, stop_date)
 
 
 
