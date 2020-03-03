002c9f9639941e0db5d47f5bd88ed5842a0ffad4
David Thole <david@thedarktrumpet.com>
2020-03-03 09:42:36 -0600

    Starting to tie the individual parts of the project together under a class like structure so we can get elements

--- togglconsumer/query/projects.py
+++ togglconsumer/query/projects.py
@@ -1,11 +1,17 @@
 import json
 from togglconsumer.query.settings import Settings
+from . import base
+from .settings import Settings
+
+class Projects (base):
+    def __init__(self):
+        pass
 
 
-def GetProjects():
-    client = Settings().toggl_client
-    responseText = client.get_projects()
-    return json.loads(responseText.text)
+    def GetProjects(self):
+        client = Settings().toggl_client
+        responseText = client.get_projects()
+        return json.loads(responseText.text)
 
 
 print(GetProjects())
