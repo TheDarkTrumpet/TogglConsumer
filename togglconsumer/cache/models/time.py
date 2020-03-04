ebd77e82c99329190c5946a2b52a39de115cfe5d
David Thole <david@thedarktrumpet.com>
2020-03-03 18:09:30 -0600

    Adding a new Time property

---
+++ togglconsumer/cache/models/time.py
@@ -0,0 +1,40 @@
+from sqlalchemy import Column, Integer, String, Boolean, DateTime, DECIMAL
+from togglconsumer.cache.models.base import Base
+
+
+class Time(Base):
+    __tablename__ = 'Times'
+
+    id = Column(Integer, primary_key=True)
+    pid = Column(Integer)
+    tid = Column(Integer)
+    uid = Column(Integer)
+    description = Column(String(500))
+    start = Column(DateTime)
+    end = Column(DateTime)
+    updated = Column(DateTime)
+    dur = Column(Integer)
+    user = Column(String(60))
+    use_stop = Column(Boolean)
+    client = Column(String(100))
+    project = Column(String(100))
+    project_color = Column(String(5))
+    project_hex_color = Column(String(20))
+    task = Column(String(500))
+    billable = Column(DECIMAL)
+    is_billable = Column(Boolean)
+    cur = Column(String(30))
+    # Tags
+
+    def __init__(self, input_dictionary=None):
+        if input_dictionary is not None:
+            self.set_attributes(input_dictionary)
+
+    def __repr__(self):
+        return "<Project(id='%i' name='%s)>" % (self.id, self.name)
+
+    def set_attributes(self, input_dictionary):
+        for key, value in input_dictionary.items():
+            setattr(self, key, value)
+            #if self.__getattribute__(key) is not None:
+            #    setattr(self, key, value)
--- togglconsumer/query/projects.py
+++ togglconsumer/query/projects.py
@@ -1,18 +1,34 @@
 import json
+from datetime import date, datetime, timedelta
 from togglconsumer.cache.models.project import Project
 from .base import Base, Elements
 
 class Projects (Base):
     def __init__(self):
         self.projects = []
+        self.time_entries = []
+        self.with_times = False
         Base.__init__(self)
 
     def load(self):
         projectsj = self._getprojects()
         for p in projectsj:
             project = Project(p)
+            if self.with_times:
+                pass
             self.projects.append(project)
 
     def _getprojects(self):
         responseText = self.client.get_projects()
         return json.loads(responseText.text)
+
+    @classmethod
+    def with_time(cls, start, end):
+        c = cls()
+        return c
+
+    @classmethod
+    def with_time(cls, days_before):
+        stop_date = date.now()
+        start_date = stop_date - timedelta(days=days_before)
+        return cls.with_time(start_date, stop_date)
